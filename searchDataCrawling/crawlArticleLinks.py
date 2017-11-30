5774ec9f9301230ada764074b1a157fa4b1704a9
easonsun <ysun73@illinois.edu>
2017-11-30 15:22:38 -0600

    label mapping from Snopes to 0/1 configured

--- searchDataCrawling/crawlArticleLinks.py
+++ searchDataCrawling/crawlArticleLinks.py
@@ -15,30 +15,33 @@ then search and record article links for each claim
 scrape the main text of each article, chop it every 4 sentences
 store the snippets in a seperte file named as the idx
 '''
+
+class InvalidCase:
+	def	__init__(self):
+		self.number_empty_claim = 0
+		self.number_empty_rating = 0
+		self.number_empty_both = 0
+
+all_ratings = set()
+
 def main():
+	invalidCase = InvalidCase()
 	f = open('../snopesDataCrawling/articles_all.json')
 	fr = open('../all ratings.txt', 'w')
 	calim_rating = json.load(f)
-	all_ratings = set()
-	number_empty_claim = 0
+
 	for item in calim_rating:
 		claim = item['claim']
-		if claim == '':
-			number_empty_claim += 1
 		rating = item['rating']
+		check_empty(claim, rating, invalidCase)
 		claim, x = clean_sentence(claim)
 		rating, rating_ = clean_sentence(rating)
-		if rating not in all_ratings:
-			all_ratings.add(rating)
-			if 'FRAUD' in rating:
-				print claim
-			fr.write(rating+'\n')
-			print(rating_)
-		#cred = map_rating_cred(rating)
+		check_rating(rating, all_ratings, fr)
+		cred = map_rating_cred(rating)
 		item['claim'] = claim
 		item['rating'] = rating
-		#item['cred'] = cred
-	print(number_empty_claim)
+		item['cred'] = cred
+
 
 #search_results = google.search(self.claim, num_page)
 main()
--- searchDataCrawling/util.py
+++ searchDataCrawling/util.py
@@ -1,11 +1,27 @@
+def check_empty(claim, rating, invalidCase):
+	if claim == '' and rating == '':
+		invalidCase.number_empty_both += 1
+	if claim == '':
+		invalidCase.number_empty_claim += 1
+	if rating == '':
+		invalidCase.number_empty_rating += 1
+
+def check_rating(rating, all_ratings, fr):
+    if rating not in all_ratings:
+        all_ratings.add(rating)
+        fr.write(rating + '\n')
+
 def clean_sentence(sen):
 	import string
 	printable = set(string.printable[:-5])
 	cleaned = filter(lambda x: x in printable, sen)
-	if cleaned == '. . FALSE.':
-		print sen
 	return cleaned, sen
-'''
+
 def map_rating_cred(rating):
-	if (rating)
-'''
+    if (('FALSE' in rating and 'MIXTURE' not in rating and 'PHOTOGRAPHS' not in rating)
+        or 'MISATTRIBUTED' in rating or 'INCORRECTLY ATTRIBUTED' in rating
+        or 'UNPROVEN' in rating):
+        return 1
+    elif (('TRUE' in rating and 'PARTLY' not in rating and 'OUTDATED' not in rating)
+        or ('CORRECTLY ATTRIBUTED' in rating)):
+        return 0
